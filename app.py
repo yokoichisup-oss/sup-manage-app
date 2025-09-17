bb9d636c40e45c02ca731d97e6a04890552114a3
yokoichisup-oss <yokoichisup@gmail.com>
2025-09-17 10:31:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ import click
 import random
 import re # 自然順ソートのために正規表現ライブラリをインポート
 from sqlalchemy.orm import joinedload, subqueryload
+
+from .models import Practice, Session, User, Attendance, Board, Transport 
+
+# もしくは、Sessionだけ追加
+from .models import Session
+
 # JSTタイムゾーンの定義 (UTC+9)
 JST = timezone(timedelta(hours=+9), 'JST')
 
@@ -839,6 +845,7 @@ def delete_announcement(announcement_id):
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
 
